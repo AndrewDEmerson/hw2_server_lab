1df272bb9288598a8052813858ccc456a7bb5974
Andrew Emerson <32277829+AndrewDEmerson@users.noreply.github.com>
2022-04-12 16:10:14 -0700

    servercode

--- main.py
+++ main.py
@@ -4,30 +4,36 @@ import sys # In order to terminate the program
 serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a sever socket
 #Fill in start
+serverSocket.bind(('', 8000))
+serverSocket.listen(1)
 #Fill in end
 while True:
     #Establish the connection
     print('Ready to serve...')
-    connectionSocket, addr = #Fill in start #Fill in end
+    connectionSocket, addr = serverSocket.accept()
     try:
-        message = #Fill in start #Fill in end
+        message = connectionSocket.recv(4096)
         filename = message.split()[1]
+        print(filename)
         f = open(filename[1:])
-        outputdata = #Fill in start #Fill in end
+        outputdata = f.read()
         #Send one HTTP header line into socket
         #Fill in start
+        connectionSocket.sendall(bytes('\nHTTP/1.1 200 OK\n\n','UTF-8'))
         #Fill in end
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
-        connectionSocket.send(outputdata[i].encode())
+            connectionSocket.send(outputdata[i].encode())
         connectionSocket.send("\r\n".encode())
         connectionSocket.close()
     except IOError:
         #Send response message for file not found
         #Fill in start
+        connectionSocket.sendall(bytes('\nHTTP/1.1 404 Not Found\n\n', 'UTF-8'))
         #Fill in end
         #Close client socket
         #Fill in start
-        #Fill in end
+        connectionSocket.close()
+        # #Fill in end
 serverSocket.close()
 sys.exit()#Terminate the program after sending the corresponding data
